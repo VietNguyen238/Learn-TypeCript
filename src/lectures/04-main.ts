e1318d8b80b322d12b56de70f583c599733ff3d7
Nguyen Quoc Viet <vietdz2308@gmail.com>
2024-05-28 13:57:43 +0700

    Learn destructuring

--- src/lectures/04-main.ts
+++ src/lectures/04-main.ts
@@ -1,3 +1,34 @@
 export function isPositive(n: number) {
   return n > 0;
 }
+
+const myInfomation1 = {
+  id: 1,
+  firstName: 'Viet',
+  LastName: 'Nguyen',
+  address: 'Da Nang',
+};
+const myInfomation2 = { ...myInfomation1, fullName: 'Nguyen Viet' };
+console.log(myInfomation2);
+
+const { firstName, ...rest } = myInfomation2;
+console.log('firstName', firstName);
+console.log('rest', rest);
+
+const myInfomation3 = [
+  {
+    id: 1,
+    firstName: 'Viet',
+    LastName: 'Nguyen',
+    address: 'Da Nang',
+  },
+  {
+    id: 2,
+    firstName: 'Nam',
+    LastName: 'Cao',
+    address: 'Da Nang',
+  },
+];
+const newList = { ...myInfomation3 };
+newList[0].firstName = 'Hau';
+console.log(newList);
